0e72a47dd5367b2377e287fef6b18403b555b200
Sharun Raj Nambayil <sharunraj39@gmail.com>
2023-06-05 21:15:06 +0530

    login signup backend

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ const App = () => {
     <div className='App'>
     <Router>
       <Routes>
-        <Route exact path='/Results' element={<Results />}/>
         <Route exact path='/' element={<Home />} />
+        <Route exact path='/Results' element={<Results />}/>
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/wishlist' element={<Wishlist/>}/>
